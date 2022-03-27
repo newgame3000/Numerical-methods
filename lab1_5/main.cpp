4c43ec9bfa7bd8964328eda2817cf8bc2d1d9115
newgame3000 <albert19411380@gmail.com>
2022-03-27 14:07:45 +0300

    QR algorithm

---
+++ lab1_5/main.cpp
@@ -0,0 +1,145 @@
+#include <iostream>
+#include "matrix.hpp"
+#include <complex>
+
+using namespace std;
+
+
+double NormVector(vector<double> x) {
+    int sum = 0;
+    for (uint i = 0; i < x.size(); ++i) {
+        sum += x[i] * x[i];
+    }
+
+    return sqrt(sum);
+}
+
+vector<double> Calculation_eps1(matrix<double> m, int n) {
+    vector<double> sum(n);
+    for (int i = 0; i < n; ++i) {
+        for (int j = i + 1; j < n; ++j) {
+            sum[i] += m[i][j] * m[i][j];
+        }
+        sum[i] = sqrt(sum[i]);
+    }
+    return sum;
+}
+
+
+vector<double> Calculation_eps2(matrix<double> m, int n) {
+    vector<double> sum(n);
+    for (int i = 0; i < n; ++i) {
+        for (int j = i + 2; j < n; ++j) {
+            sum[i] += m[i][j] * m[i][j];
+        }
+        sum[i] = sqrt(sum[i]);
+    }
+    return sum;
+}
+
+pair<complex<double>, complex<double>> Calculation_lambda_complex(matrix<double> m, int j, bool &con, double eps) {
+    double re;
+    double im;
+
+    double d = (m[j][j] + m[j + 1][j + 1]) * (m[j][j] + m[j + 1][j + 1]) - 4 * (m[j][j] * m[j + 1][j + 1] - m[j][j + 1] * m[j + 1][j]);
+
+
+    re = (m[j][j] + m[j + 1][j + 1]) / 2;
+    im = sqrt(abs(d)) / 2;
+
+    pair<complex<double>, complex<double>> res;
+
+    if ((d > eps) || (im < eps)) {
+        con = true;
+    }
+
+    res.first = complex<double>(re, im);
+    res.second = complex<double>(re, -im);
+
+    return res;
+
+}
+
+void Calculation_lambda(matrix<double> m, vector<complex<double>> &lambda, int n, double eps, bool &con) {
+    for (int i = 0; i < n; ++i) {
+        if ((i == n - 1) || (abs(m[i + 1][i]) < eps)) {
+            lambda[i] = m[i][i];
+        } else {            pair<complex<double>, complex<double>> k = Calculation_lambda_complex(m, i, con, eps);
+
+            if (!con) {
+                lambda[i] = k.first;
+                i += 1;
+                lambda[i] = k.second;
+            }
+        }
+    }
+}
+
+void QR_algorithm(matrix<double> m, int n, double eps, vector<complex<double>> &lambda, int &iter) {
+
+    matrix<double> q(n, n);
+    matrix<double> r(n, n);
+
+    vector<complex<double>> oldlambda(n, 1e18);
+
+    bool flag12 = true;
+    bool flag3 = true;
+
+    while(flag12 || flag3) {
+
+        iter += 1;
+
+        m.QR(q, r);
+        m = r * q;
+
+        if ((NormVector(Calculation_eps1(m, n)) < eps) || (NormVector(Calculation_eps2(m, n)) < eps)) {
+            flag12 = false;
+        }
+
+        bool con = false;
+
+        Calculation_lambda(m, lambda, n, eps, con);
+
+        if (con) {
+            continue;
+        }
+
+        vector<double> eps3(n);
+
+        for (int i = 0; i < n; ++i) {
+            eps3[i] = abs(abs(lambda[i]) - abs(oldlambda[i]));   
+        }
+
+        if (NormVector(eps3) < eps) {
+            flag3 = false;
+        }
+
+        oldlambda = lambda;
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+
+	matrix<double> m(n, n);
+    m.Read();
+
+    double eps;
+    cin >> eps;
+
+    vector<complex<double>> lambda(n);
+
+    int iter = 0;
+
+    QR_algorithm(m, n, eps, lambda, iter);
+
+    cout << "Собственные значения\n";
+    for (int i = 0; i < n; ++i) {
+        cout << lambda[i] << " ";
+    }
+    cout << endl;
+
+    cout << "Количество итераций\n";
+    cout << iter << endl;
+}
---
+++ lab1_5/matrix.hpp
@@ -0,0 +1,279 @@
+#ifndef MATRIX_HPP
+#define MATRIX_HPP
+
+#include<iostream>
+#include <vector>
+#include <string>
+#include <iomanip>
+#include <math.h>
+
+using namespace std;
+
+template <class T>
+class matrix {
+private: 
+    vector<vector<T>> data;
+
+    int Sign(T a){
+        if (a > 0) {
+            return 1;
+        } else {
+            if (a < 0) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+    }
+
+    double Norm(vector<T> b, int k) {
+        double ans = 0;
+
+        for (uint i = k; i < b.size(); ++i) {
+            ans += b[i] * b[i];
+        }
+
+        return (sqrt(ans));
+    }
+
+    matrix<T> VVT(vector<T> v, vector<T> vt) {
+        matrix<T> ans(v.size(), v.size());
+
+        for (uint i = 0; i < v.size(); ++i) {
+            for (uint j = 0; j < vt.size(); ++j) {
+                ans[i][j] = v[i] * vt[j];
+            }
+        }
+
+        return ans;
+    }
+
+    T VTV(vector<T> v, vector<T> vt) {
+        T ans = 0;
+        for (uint i = 0; i < v.size(); ++i) {
+                ans += v[i] * vt[i];
+        }
+
+        return ans;
+    }
+
+public:
+
+    matrix(int n, int m) {
+        data = vector<vector<T>>(n, vector<T>(m));
+    }
+
+    matrix(const matrix<T> &a) {
+        data = a.data;
+    }
+
+    vector<T>& operator[](int i) {
+        return data[i];
+    }
+
+    void Read() {
+        for (uint i = 0; i < data.size(); ++i) {
+            for (uint j = 0; j < data[i].size(); ++j) {
+                cin >> data[i][j];
+            }
+        }
+    }
+
+    void Print() {
+        cout.precision(3);
+        cout << fixed;
+        for (uint i = 0; i < data.size(); ++i) {
+            for (uint j = 0; j < data[i].size(); ++j) {
+                cout << setw(6) << data[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    uint Rows_count() const{
+        return data.size();
+    }
+
+    uint Columns_count() const{
+        return data[0].size();
+    }
+
+    void Swap_rows(int i, int j) {
+        vector<T> s = data[i];
+        data[i] = data[j];
+        data[j] = s;
+    }
+
+    void Swap_columns(int i, int j) {
+        vector<T> s(data.size());
+        for (uint k = 0; k < data.size(); ++k) {
+            s[k] = data[k][i];
+        }
+
+        for (uint k = 0; k < data.size(); ++k) {
+            data[k][i] = data[k][j];
+        }
+
+        for (uint k = 0; k < data.size(); ++k) {
+            data[k][j] = s[k];
+        }    
+    }
+
+    void Fill_in_the_column(vector<T> a, int k) {
+        for (uint i = 0; i < a.size(); ++i) {
+            data[i][k] = a[i];
+        }
+    }
+
+    double Norm() {
+        double n = 0;
+        for (uint j = 0; j < data[0].size(); ++j) {
+            n += abs(data[0][j]); 
+        }
+
+        for (uint i = 0; i < data.size(); ++i) {
+            double k = 0;
+            for (uint j = 0; j < data[i].size(); ++j) {
+                k += abs(data[i][j]); 
+            }
+            if (k > n) {
+                n = k;
+            }
+        }
+
+        return n;
+    }
+
+    double SqrtDiagonal() {
+        double res = 0;
+        for (uint i = 0; i < data.size(); ++i) {
+            for (uint j = i + 1; j < data[i].size(); ++j) {
+                res += data[i][j] * data[i][j];
+            }
+        }
+        res = sqrt(res);
+        return res;
+    }
+
+    matrix<T> Transpose() {
+        matrix<T> res(data[0].size(), data.size()) ;
+        for (uint i = 0; i < data.size(); ++i) {
+            for (uint j = 0; j < data[i].size(); ++j) {
+               res[i][j] = data[j][i];
+            }
+        }
+        return res;
+    }
+
+    void Identity() {
+        for (uint i = 0; i < data.size(); ++i) {
+            data[i][i] = 1;
+        }
+    }
+
+    void QR(matrix<T> &q, matrix<T> &r) {
+        r.data = data;
+        q = matrix<T>(r.Rows_count(), r.Columns_count());
+        q.Identity();
+
+        matrix<T> E(r.Rows_count(), r.Columns_count());
+        matrix<T> H(r.Rows_count(), r.Columns_count());
+
+        E.Identity();
+
+        for (uint k = 0; k < r.Columns_count() - 1; ++k) {
+            vector<T> b(r.Rows_count());
+            for (uint i = 0; i < r.Rows_count(); ++i) {
+                b[i] = r[i][k];
+            }
+            vector<T> v(r.Rows_count());
+
+            for (uint i = 0; i < r.Rows_count(); ++i) {
+                
+                v[i] = b[i];
+
+                if (i < k) {
+                    v[i] = 0;
+                }
+
+                if (i == k) {
+                    v[i] += Sign(b[i]) * Norm(b, k);
+                }
+            }
+
+
+            H = E  - 2 * (VVT(v, v) / VTV(v, v));
+
+            q = q * H;
+            r = H * r;
+        }
+    }
+
+
+    matrix<T>& operator=(const matrix<T> &l) {
+        for (uint i = 0; i < data.size(); ++i) {
+            data[i] = l.data[i];
+        }
+        return *this;
+    }
+
+    friend matrix<T> operator*(const matrix<T> &l, const matrix<T> &r) {
+        
+        matrix<T> res(l.Rows_count(), r.Columns_count());
+
+        for (uint i = 0; i < l.Rows_count(); ++i) {
+            for (uint j = 0; j < r.Columns_count(); ++j) {
+                for (uint t = 0; t < l.Columns_count(); ++t) {
+                    res.data[i][j] += l.data[i][t] * r.data[t][j];
+                }
+            }
+        }
+        return res;
+    }
+
+    friend vector<T> operator*(const matrix<T> &l, const vector<T> &r) {
+        vector<T> res(l.Rows_count());
+
+        for (uint i = 0; i < l.Rows_count(); ++i) {
+                for (uint t = 0; t < l.Columns_count(); ++t) {
+                    res.data[i] += l.data[i][t] * r[t];
+            }
+        }
+        return res;
+    }
+
+    friend matrix<T> operator/(const matrix<T> &l, const T &r) {
+        matrix<T> res(l.Rows_count(), l.Columns_count());
+        for (uint i = 0; i < l.Rows_count(); ++i) {
+            for (uint j = 0; j < l.Columns_count(); ++j) {
+                res.data[i][j] = l.data[i][j] / r;
+            }
+        }
+
+        return res;
+    }
+
+    friend matrix<T> operator*(const T &r, const matrix<T> &l) {
+        matrix<T> res(l.Rows_count(), l.Columns_count());
+        for (uint i = 0; i < l.Rows_count(); ++i) {
+            for (uint j = 0; j < l.Columns_count(); ++j) {
+                res.data[i][j] = l.data[i][j] * r;
+            }
+        }
+        return res;
+    }
+
+    friend matrix<T> operator-(const matrix<T> &l, const matrix<T> &r) {
+        
+        matrix<T> res(l.Rows_count(), l.Columns_count());
+
+        for (uint i = 0; i < l.Rows_count(); ++i) {
+            for (uint j = 0; j < r.Columns_count(); ++j) {
+                res.data[i][j] = l.data[i][j] - r.data[i][j];
+            }
+        }
+        return res;
+    }
+};
+
+#endif
